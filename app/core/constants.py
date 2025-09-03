c316af8b43555a77af080055f9bdf640625f1761
RulCab <167165316+RulCab@users.noreply.github.com>
2025-09-02 18:19:49 -0600

    Create constants.py

---
+++ app/core/constants.py
@@ -0,0 +1,19 @@
+TOPICS = [
+    "Expensive perfumes are always better than cheap ones",
+    "Sweet fragrances are masculine, and fresh ones are feminine",
+    "Dior Sauvage is the only perfume you should wear",
+    "Niche fragrances are just an unnecessary trend",
+    "A perfume can change your life",
+    "Vintage fragrances are the only real option",
+    "Clones should be illegal",
+    "Longevity is everything",
+    "You can't wear fresh perfumes in winter",
+    "Clothing and style don’t matter, only perfume does",
+]
+
+ARGUMENT_STYLES = [
+    "Historical: Throughout history, civilizations have valued high-quality perfumes...",
+    "Scientific: Studies have shown that luxury fragrances contain more refined ingredients...",
+    "Emotional: An expensive perfume is not just a scent, it's a bottled memory...",
+    "Sarcastic: Sure, go ahead and use a cheap perfume if you want to smell like a car air freshener...",
+]
